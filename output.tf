b5058b1445305e35fb9a5d7b19d47436497a9549
Saurabh <saurabh.d2106@gmail.com>
2025-04-07 15:45:37 +0530

    Lab Solution

---
+++ output.tf
@@ -0,0 +1,11 @@
+output "public_ip" {
+
+  value = [for instance in azurerm_linux_virtual_machine.vm : instance.public_ip_address]
+
+}
+
+output "private_ip" {
+
+  value = [for instance in azurerm_linux_virtual_machine.vm : instance.private_ip_address]
+
+}
